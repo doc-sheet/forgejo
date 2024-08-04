f206dbf5baff513740e8ceaf971b2d08d9c87fbc
Gergely Nagy <forgejo@gergo.csillger.hu>
2024-08-04 16:08:34 +0200

    activitypub: Implement user activity federation
    This is an implementation of user activity federation, as rendered Note
    objects. Anything that would generate an activity in an RSS feed, will
    now also generate an ActivityPub Note too, which will be federated out
    to followers.
    
    Because this is purely informational content, and receivers should not
    need to act on them, they're sent out as Notes, so that they can be
    displayed and interacted with via other Fedi clients.
    
    Naturally, this also implements following a user.
    
    Signed-off-by: Gergely Nagy <forgejo@gergo.csillger.hu>

---
+++ services/federation/person_inbox_create.go
@@ -0,0 +1,54 @@
+// Copyright 2024 The Forgejo Authors. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+package federation
+
+import (
+	"fmt"
+	"net/http"
+
+	"code.gitea.io/gitea/models/forgefed"
+	fm "code.gitea.io/gitea/modules/forgefed"
+	"code.gitea.io/gitea/modules/log"
+	"code.gitea.io/gitea/modules/validation"
+	context_service "code.gitea.io/gitea/services/context"
+
+	ap "github.com/go-ap/activitypub"
+)
+
+func processPersonInboxCreate(ctx *context_service.APIContext, activity *ap.Activity) {
+	// This, and the other two instances of nolint here are to disable the
+	// "struct literal uses unkeyed fields" check. In all three cases, this is
+	// an embedded type, we *want to* use unkeyed fields.
+	createAct := fm.ForgeUserActivity{*activity} //nolint
+
+	if res, err := validation.IsValid(createAct); !res {
+		log.Error("Invalid user activity: %v", activity)
+		ctx.Error(http.StatusNotAcceptable, "Invalid user activity", err)
+		return
+	}
+
+	if createAct.Object.GetType() != ap.NoteType {
+		log.Error("Invalid object type for Create activity: %v", createAct.Object.GetType())
+		ctx.Error(http.StatusNotAcceptable, "Invalid object type for Create activity", fmt.Errorf("Invalid object type for Create activity: %v", createAct.Object.GetType()))
+		return
+	}
+
+	a := createAct.Object.(*ap.Object)
+	userActivity := fm.ForgeUserActivityNote{*a} //nolint
+	act := fm.ForgeUserActivity{*activity}       //nolint
+
+	actorURI := act.Actor.GetLink().String()
+	if _, _, _, err := findOrCreateFederatedUser(ctx, actorURI); err != nil {
+		log.Error("Error finding or creating federated user (%s): %v", actorURI, err)
+		return
+	}
+
+	if err := forgefed.AddUserActivity(ctx, ctx.ContextUser.ID, actorURI, &userActivity); err != nil {
+		log.Error("Unable to record activity: %v", err)
+		ctx.Error(http.StatusInternalServerError, "Unable to record activity", err)
+		return
+	}
+
+	ctx.Status(http.StatusNoContent)
+}
